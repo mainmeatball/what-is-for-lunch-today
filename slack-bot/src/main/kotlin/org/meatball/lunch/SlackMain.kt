60fce1a0b4567a9ba148d4237ce74ab0b21702cf
sstepanov <sstepanov@solanteq.com>
2024-01-24 12:41:58 +0300

    Remove app.pid generation

--- slack-bot/src/main/kotlin/org/meatball/lunch/SlackMain.kt
+++ slack-bot/src/main/kotlin/org/meatball/lunch/SlackMain.kt
@@ -1,11 +1,7 @@
 package org.meatball.lunch
 
 import org.meatball.lunch.app.SlackApplication
-import org.meatball.lunch.env.writePidToFile
 
 fun main() {
-    val pid = ProcessHandle.current().pid()
-    writePidToFile(pid)
-
     SlackApplication().run()
 }
