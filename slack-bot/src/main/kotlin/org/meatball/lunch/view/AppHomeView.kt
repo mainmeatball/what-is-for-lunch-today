66d8f28440280ec180cc447adf6fbf9cb5137d6b
sstepanov <sstepanov@solanteq.com>
2024-03-06 17:21:44 +0300

    Add tomorrow lunch info & refactor app home view a bit

--- slack-bot/src/main/kotlin/org/meatball/lunch/view/AppHomeView.kt
+++ slack-bot/src/main/kotlin/org/meatball/lunch/view/AppHomeView.kt
@@ -1,14 +1,23 @@
 package org.meatball.lunch.view
 
+import com.slack.api.model.kotlin_extension.block.SectionBlockBuilder
+import com.slack.api.model.kotlin_extension.block.dsl.LayoutBlockDsl
 import com.slack.api.model.kotlin_extension.view.blocks
 import com.slack.api.model.view.View
 import com.slack.api.model.view.Views.view
 import org.meatball.lunch.food.FoodData
 import org.meatball.lunch.singletone.lunchService
+import java.time.DayOfWeek
 import java.time.LocalDate
 
 fun homeTabView(userLunchName: String?, today: LocalDate, foodList: List<FoodData>? = null, msg: String? = null): View {
     val todayLunch = msg?.let { emptyList() } ?: foodList ?: lunchService.getFoodList(userLunchName!!, today) ?: emptyList()
+    val tomorrow = today.plusDays(1L)
+    val tomorrowLunch = if (tomorrow.isWorkingDay()) {
+        msg?.let { emptyList() } ?: foodList ?: lunchService.getFoodList(userLunchName!!, tomorrow) ?: emptyList()
+    } else {
+        emptyList()
+    }
 
     val view = view {
         it
@@ -19,37 +28,50 @@ fun homeTabView(userLunchName: String?, today: LocalDate, foodList: List<FoodDat
                         markdownText(msg)
                     }
                 } else {
-                    when {
-                        todayLunch.isEmpty() -> {
-                            header {
-                                text("Сегодня")
-                            }
-                            section {
-                                markdownText("*Вы на сегодня ничего не заказывали*")
-                            }
-                        }
-                        else -> {
-                            header {
-                                text("Сегодня")
-                            }
-                            todayLunch.map {
-                                section {
-                                    markdownText("""
-                                    *${it.name}*
-                                    
-                                    *   К   |  Б  |  Ж  |  У*
-                                    ${it.calories} | ${it.protein} | ${it.fat} | ${it.carbs}
-                                    
-                                    *Вес:* ${it.weight} г.
-                                """.trimIndent())
-                                }
-                                divider()
-                            }
-                        }
-                    }
+                    toMarkdown(todayLunch, "сегодня")
+                    toMarkdown(tomorrowLunch, "завтра")
                 }
             }
     }
 
     return view
+}
+
+private fun LocalDate.isWorkingDay() = dayOfWeek !in setOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
+
+private fun LayoutBlockDsl.noFoodOrderedFor(day: String) {
+    section {
+        markdownText("*Вы ничего не заказывали на $day*")
+    }
+}
+
+private fun SectionBlockBuilder.foodInfo(foodData: FoodData) {
+    markdownText("""
+        *${foodData.name}*
+                                    
+        *   К   |  Б  |  Ж  |  У*
+        ${foodData.calories} | ${foodData.protein} | ${foodData.fat} | ${foodData.carbs}
+        
+        *Вес:* ${foodData.weight} г.
+        """.trimIndent()
+    )
+}
+
+private fun String.upFirstChar() = replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
+
+private fun LayoutBlockDsl.toMarkdown(foodData: List<FoodData>, dayName: String) {
+    header {
+        text(dayName.upFirstChar())
+    }
+    when {
+        foodData.isEmpty() -> noFoodOrderedFor(dayName)
+        else -> {
+            foodData.map {
+                section {
+                    foodInfo(it)
+                }
+                divider()
+            }
+        }
+    }
 }
